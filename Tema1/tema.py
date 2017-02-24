190ba4e3d4a2b56e6a6693d66d8f8363a00416f7
dev-alberto <alberto.cieri@info.uaic.ro>
2017-02-24 12:49:43 +0200

    Small ajustments

--- Tema1/tema.py
+++ Tema1/tema.py
@@ -41,43 +41,66 @@ print(mul_assoc(100))
 
 ### Exercitiul 3 ###
 
-c = [0.16666666666666666666666666666667, 0.00833333333333333333333333333333, 1.984126984126984126984126984127e-4,
-     2.7557319223985890652557319223986e-6,
-     2.5052108385441718775052108385442e-8, 1.6059043836821614599392377170155e-10]
+c = [1/math.factorial(3), 1/math.factorial(5), 1/math.factorial(7), 1/math.factorial(9),
+     1 / math.factorial(11), 1/math.factorial(13)]
 
 
-def first_poly(x):
+def first_poly_horner(x):
     y = x ** 2
     return x * (1 + y * (-c[0] + c[1] * y))
 
 
-def second_poly(x):
+def second_poly_horner(x):
     y = x ** 2
     return x * (1 + y * (-c[0] + y * (c[1] - c[2] * y)))
 
 
-def third_poly(x):
+def third_poly_horner(x):
     y = x ** 2
     return x * (1 + y * (-c[0] + y * (c[1] + y * (-c[2] + c[3] * y))))
 
 
-def fourth_poly(x):
+def fourth_poly_horner(x):
     y = x ** 2
     return x * (1 + y * (-0.166 + y * (0.00833 + y * (-c[2] + c[3] * y))))
 
 
-def fifth_poly(x):
+def fifth_poly_horner(x):
     y = x ** 2
     return x * (1 + y * (-c[0] + y * (c[1] + y * (-c[2] + y * (c[3] - c[4] * y)))))
 
 
-def sixth_poly(x):
+def sixth_poly_horner(x):
     y = x ** 2
     return x * (1 + y * (-c[0] + y * (c[1] + y * (-c[2] + y * (c[3] + y * (-c[4] + c[5] * y))))))
 
 
+def first_poly(x):
+    return x - c[0]*x**3 + c[1]*x**5
+
+
+def second_poly(x):
+    return x - c[0]*x**3 + c[1]*x**5 - c[2]*x**7
+
+
+def third_poly(x):
+    return x - c[0]*x**3 + c[1]*x**5 - c[2]*x**7 + c[3]*x**9
+
+
+def fourth_poly(x):
+    return x - 0.166*x**3 + 0.00833*x**5 - c[2]*x**7 + c[3]*x**9
+
+
+def fifth_poly(x):
+    return x - c[0]*x**3 + c[1]*x**5 - c[2]*x**7 + c[3]*x**9 - c[4]*x**11
+
+
+def sixth_poly(x):
+    return x - c[0]*x**3 + c[1]*x**5 - c[2]*x**7 + c[3]*x**9 - c[4]*x**11 + c[5]*x**13
+
+
 def sin_values(x):
-    return fifth_poly(x), second_poly(x), third_poly(x), fourth_poly(x), fifth_poly(x), sixth_poly(x)
+    return first_poly(x), second_poly(x), third_poly(x), fourth_poly(x), fifth_poly(x), sixth_poly(x)
 
 
 random_numbers = []
@@ -112,41 +135,50 @@ def get_best_poly():
 def test_time():
     start1 = time.time()
     for j in random_numbers:
-        first_poly(j)
+        first_poly_horner(j)
     end1 = time.time()
-    print("First poly time is " + str((end1 - start1)))
+    print("Efficient First poly time is " + str((end1 - start1)))
 
     start2 = time.time()
     for j in random_numbers:
-        second_poly(j)
+        second_poly_horner(j)
     end2 = time.time()
-    print("Second poly time is " + str((end2 - start2)))
+    print("Efficient Second poly time is " + str((end2 - start2)))
 
     start3 = time.time()
     for j in random_numbers:
-        third_poly(j)
+        third_poly_horner(j)
     end3 = time.time()
-    print("Third poly time is " + str((end3 - start3)))
+    print("Efficient Third poly time is " + str((end3 - start3)))
 
     start4 = time.time()
     for j in random_numbers:
-        fourth_poly(j)
+        fourth_poly_horner(j)
     end4 = time.time()
-    print("Fourth poly time is " + str((end4 - start4)))
+    print("Efficient Fourth poly time is " + str((end4 - start4)))
 
     start5 = time.time()
     for j in random_numbers:
-        fifth_poly(j)
+        fifth_poly_horner(j)
     end5 = time.time()
-    print("Fifth poly time is " + str((end5 - start5)))
+    print("Efficient Fifth poly time is " + str((end5 - start5)))
 
     start6 = time.time()
     for j in random_numbers:
-        sixth_poly(j)
+        sixth_poly_horner(j)
     end6 = time.time()
-    print("Sixth poly time is " + str((end6 - start6)))
+    print("Efficient Sixth poly time is " + str((end6 - start6)))
 
-print("Best poly is: ")
+    print("************")
+
+    start = time.time()
+    for j in random_numbers:
+        sixth_poly(j)
+    end = time.time()
+
+    print("Normal Sixth poly is " + str(end - start))
+
+print("Best poly by error is: ")
 print(get_best_poly())
 test_time()
 
