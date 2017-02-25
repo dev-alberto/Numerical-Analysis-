64a73c7ca5d0f6625a7669286cf7306f2c653bdc
dev-alberto <alberto.cieri@info.uaic.ro>
2017-02-25 11:36:37 +0200

    attempted buf fix

--- Tema1/tema_GUI.py
+++ Tema1/tema_GUI.py
@@ -7,99 +7,39 @@ from flask import render_template
 
 app = Flask(__name__)
 
-'''
-
-# Exe 3
-random_numbers = []
-r = 0
-while r < 100000:
-	random_numbers.append(random.uniform(-math.pi/2, math.pi/2))
-	r += 1
-
-print("Best poly is: ")
-
-c = [0.16666666666666666666666666666667, 0.00833333333333333333333333333333, 1.984126984126984126984126984127e-4,
-	 2.7557319223985890652557319223986e-6,
-	 2.5052108385441718775052108385442e-8, 1.6059043836821614599392377170155e-10]
-PA = PolynomialApproximator(c)
-
-print(PA.get_best_poly())
-
-PA.test_time()
-'''
-
-@app.context_processor
-def utility_processor():
-	def find_precision_wrapper():
-		return str(tm.find_precision())
-	def add_assoc_hardcoded_wrapper():
-		return str(tm.add_assoc(1.0))
-	def mul_assoc_hardcoded_wrapper():
-		return str(tm.mul_assoc(100))
-
-	def polyomial_hardcoded_wrapper():
-		random_numbers = []
-		r = 0
-		while r < 100000:
-			random_numbers.append(random.uniform(-math.pi/2, math.pi/2))
-			r += 1
-
-		c = [0.16666666666666666666666666666667, 0.00833333333333333333333333333333, 1.984126984126984126984126984127e-4,
-			 2.7557319223985890652557319223986e-6,
-			 2.5052108385441718775052108385442e-8, 1.6059043836821614599392377170155e-10]
-		PA = tm.PolynomialApproximator(c, random_numbers)
-
-		return str(PA.get_best_poly()) + '\n' + str(PA.test_time())
-
-	def polyomial_time_hardcoded_wrapper():
-		return str(PA.test_time())
-
-	return dict(
-		find_precision 	= find_precision_wrapper,
-		add_assoc		= add_assoc_hardcoded_wrapper,
-		mul_assoc 		= mul_assoc_hardcoded_wrapper,
-		polynomial 		= polyomial_hardcoded_wrapper
-	)
-
 @app.route('/')
 def hello_world():
-	name = 'Flask app'
-	return render_template('index.html', name=name)
+    name = 'Flask app'
+    return render_template('index.html', name=name)
 
 
 # Route handlers
 @app.route('/precision')
 def find_precision_wrapper():
-	return str(tm.find_precision())
+    return str(tm.find_precision())
+
 
 @app.route('/add')
 def add_assoc_hardcoded_wrapper():
-	return str(tm.add_assoc(1.0))
+    return str(tm.add_assoc(1.0))
+
 
 @app.route('/mul')
 def mul_assoc_hardcoded_wrapper():
-	return str(tm.mul_assoc(100))
+    return str(tm.mul_assoc(100))
+
 
 @app.route('/polynomial')
-def polyomial_hardcoded_wrapper():
-	random_numbers = []
-	r = 0
-	while r < 10000:
-		random_numbers.append(random.uniform(-math.pi/2, math.pi/2))
-		r += 1
-	
-	print("Best poly is: ")
-
-	# c = [0.16666666666666666666666666666667, 0.00833333333333333333333333333333, 1.984126984126984126984126984127e-4,
-	#      2.7557319223985890652557319223986e-6,
-	#      2.5052108385441718775052108385442e-8, 1.6059043836821614599392377170155e-10]
-	c = [1/math.factorial(3), 1/math.factorial(5), 1/math.factorial(7), 1/math.factorial(9),
-		 1 / math.factorial(11), 1/math.factorial(13)]
-	PA = tm.PolynomialApproximator(c, random_numbers)
-
-	print(PA.get_best_poly())
-
-	print(PA.test_time())
+def polynomial_hardcoded_wrapper():
+    return tm.poly_tests()
+    #return "CACA"
 
 
-	return str(PA.get_best_poly()) + '\n' + str(PA.test_time())
+@app.context_processor
+def utility_processor():
+    return dict(
+        find_precision=find_precision_wrapper,
+        add_assoc=add_assoc_hardcoded_wrapper,
+        mul_assoc=mul_assoc_hardcoded_wrapper,
+        polynomial=polynomial_hardcoded_wrapper
+    )
