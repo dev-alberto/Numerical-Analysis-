8f4bf5a31dd2c18296bf46e9fb33548c77ec0978
dev-alberto <alberto.cieri@info.uaic.ro>
2017-02-25 11:36:37 +0200

    attempted buf fix

--- Tema1/tema.py
+++ Tema1/tema.py
@@ -1,7 +1,8 @@
 import random
 import math
 import time
- 
+
+
 ### Exercitiu 1 ###
 
 def find_precision():
@@ -35,69 +36,56 @@ def mul_assoc(iterations):
 ### Exercitiul 3 ###
 
 class PolynomialApproximator:
-    
     def __init__(self, c, random_numbers):
         self.c = c
         self.random_numbers = random_numbers
-
+        self.n = len(random_numbers)
 
     def first_poly_horner(self, x):
         y = x ** 2
         return x * (1 + y * (-self.c[0] + self.c[1] * y))
 
-
     def second_poly_horner(self, x):
         y = x ** 2
         return x * (1 + y * (-self.c[0] + y * (self.c[1] - self.c[2] * y)))
-
 
     def third_poly_horner(self, x):
         y = x ** 2
         return x * (1 + y * (-self.c[0] + y * (self.c[1] + y * (-self.c[2] + self.c[3] * y))))
 
-
     def fourth_poly_horner(self, x):
         y = x ** 2
         return x * (1 + y * (-0.166 + y * (0.00833 + y * (-self.c[2] + self.c[3] * y))))
-
 
     def fifth_poly_horner(self, x):
         y = x ** 2
         return x * (1 + y * (-self.c[0] + y * (self.c[1] + y * (-self.c[2] + y * (self.c[3] - self.c[4] * y)))))
 
-
     def sixth_poly_horner(self, x):
         y = x ** 2
         return x * (1 + y * (-self.c[0] + y * (self.c[1] + y * (-self.c[2] + y * (self.c[3] + y * (-self.c[4] + self.c[5] * y))))))
 
-
     def first_poly(self, x):
-        return x - self.c[0]*x**3 + self.c[1]*x**5
-
+        return x - self.c[0] * x ** 3 + self.c[1] * x ** 5
 
     def second_poly(self, x):
-        return x - self.c[0]*x**3 + self.c[1]*x**5 - self.c[2]*x**7
-
+        return x - self.c[0] * x ** 3 + self.c[1] * x ** 5 - self.c[2] * x ** 7
 
     def third_poly(self, x):
-        return x - self.c[0]*x**3 + self.c[1]*x**5 - self.c[2]*x**7 + self.c[3]*x**9
-
+        return x - self.c[0] * x ** 3 + self.c[1] * x ** 5 - self.c[2] * x ** 7 + self.c[3] * x ** 9
 
     def fourth_poly(self, x):
-        return x - 0.166*x**3 + 0.00833*x**5 - self.c[2]*x**7 + self.c[3]*x**9
-
+        return x - 0.166 * x ** 3 + 0.00833 * x ** 5 - self.c[2] * x ** 7 + self.c[3] * x ** 9
 
     def fifth_poly(self, x):
-        return x - self.c[0]*x**3 + self.c[1]*x**5 - self.c[2]*x**7 + self.c[3]*x**9 - self.c[4]*x**11
-
+        return x - self.c[0] * x ** 3 + self.c[1] * x ** 5 - self.c[2] * x ** 7 + self.c[3] * x ** 9 - self.c[4] * x ** 11
 
     def sixth_poly(self, x):
-        return x - self.c[0]*x**3 + self.c[1]*x**5 - self.c[2]*x**7 + self.c[3]*x**9 - self.c[4]*x**11 + self.c[5]*x**13
+        return x - self.c[0] * x ** 3 + self.c[1] * x ** 5 - self.c[2] * x ** 7 + self.c[3] * x ** 9 - self.c[4] * x ** 11 + self.c[5] * x ** 13
 
-    
     def sin_values(self, x):
-        return self.first_poly(x), self.second_poly(x), self.third_poly(x), self.fourth_poly(x), self.fifth_poly(x), self.sixth_poly(x)
-
+        return self.first_poly(x), self.second_poly(x), self.third_poly(x), self.fourth_poly(x), self.fifth_poly(
+            x), self.sixth_poly(x)
 
     def compute_ith_poly_error(self, i):
         result = []
@@ -105,10 +93,8 @@ class PolynomialApproximator:
             result.append(abs(self.sin_values(self.random_numbers[j])[i] - math.sin(self.random_numbers[j])))
         return result
 
-
     def compute_ith_poly_mean_error(self, i):
-        return sum(self.compute_ith_poly_error(i))/100000
-
+        return sum(self.compute_ith_poly_error(i)) / self.n
 
     def make_error_mean_list(self):
         mean_errors = []
@@ -116,10 +102,8 @@ class PolynomialApproximator:
             mean_errors.append(self.compute_ith_poly_mean_error(j))
         return mean_errors
 
-
     def get_best_poly(self):
         return self.make_error_mean_list().index(min(self.make_error_mean_list())) + 1
-
 
     def test_time(self):
         output = ''
@@ -175,9 +159,22 @@ class PolynomialApproximator:
         return output
 
 
+def poly_tests():
+    random_numbers = []
+    r = 0
+    while r < 1000:
+        random_numbers.append(random.uniform(-math.pi / 2, math.pi / 2))
+        r += 1
+    c = [1 / math.factorial(3), 1 / math.factorial(5), 1 / math.factorial(7), 1 / math.factorial(9),
+         1 / math.factorial(11), 1 / math.factorial(13)]
+    PA = PolynomialApproximator(c, random_numbers)
+
+    print(PA.make_error_mean_list())
+    print(PA.get_best_poly())
+
+    return str(PA.get_best_poly()) + '\n' + str(PA.test_time())
 
 if __name__ == "__main__":
-
     # Exe 1
     print("Smallest u that satisfies said property is " + str(find_precision()))
 
@@ -188,21 +185,5 @@ if __name__ == "__main__":
     print(mul_assoc(100))
 
     # Exe 3
-    random_numbers = []
-    r = 0
-    while r < 100000:
-        random_numbers.append(random.uniform(-math.pi/2, math.pi/2))
-        r += 1
-    
-    print("Best poly is: ")
+    poly_tests()
 
-    # c = [0.16666666666666666666666666666667, 0.00833333333333333333333333333333, 1.984126984126984126984126984127e-4,
-    #      2.7557319223985890652557319223986e-6,
-    #      2.5052108385441718775052108385442e-8, 1.6059043836821614599392377170155e-10]
-    c = [1/math.factorial(3), 1/math.factorial(5), 1/math.factorial(7), 1/math.factorial(9),
-         1 / math.factorial(11), 1/math.factorial(13)]
-    PA = PolynomialApproximator(c, random_numbers)
-
-    print(PA.get_best_poly())
-
-    PA.test_time()
