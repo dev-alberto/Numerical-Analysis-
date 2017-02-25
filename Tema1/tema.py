97fb37dff8613e3897ab18e56136782c60c58e8a
dev-alberto <alberto.cieri@info.uaic.ro>
2017-02-25 23:12:50 +0200

    fixed bug

--- Tema1/tema.py
+++ Tema1/tema.py
@@ -41,30 +41,6 @@ class PolynomialApproximator:
         self.random_numbers = random_numbers
         self.n = len(random_numbers)
 
-    def first_poly_horner(self, x):
-        y = x ** 2
-        return x * (1 + y * (-self.c[0] + self.c[1] * y))
-
-    def second_poly_horner(self, x):
-        y = x ** 2
-        return x * (1 + y * (-self.c[0] + y * (self.c[1] - self.c[2] * y)))
-
-    def third_poly_horner(self, x):
-        y = x ** 2
-        return x * (1 + y * (-self.c[0] + y * (self.c[1] + y * (-self.c[2] + self.c[3] * y))))
-
-    def fourth_poly_horner(self, x):
-        y = x ** 2
-        return x * (1 + y * (-0.166 + y * (0.00833 + y * (-self.c[2] + self.c[3] * y))))
-
-    def fifth_poly_horner(self, x):
-        y = x ** 2
-        return x * (1 + y * (-self.c[0] + y * (self.c[1] + y * (-self.c[2] + y * (self.c[3] - self.c[4] * y)))))
-
-    def sixth_poly_horner(self, x):
-        y = x ** 2
-        return x * (1 + y * (-self.c[0] + y * (self.c[1] + y * (-self.c[2] + y * (self.c[3] + y * (-self.c[4] + self.c[5] * y))))))
-
     def first_poly(self, x):
         return x - self.c[0] * x ** 3 + self.c[1] * x ** 5
 
@@ -105,47 +81,55 @@ class PolynomialApproximator:
     def get_best_poly(self):
         return self.make_error_mean_list().index(min(self.make_error_mean_list())) + 1
 
+    def poly_horner_eval(self, poly, x):
+        """Evaluates given polynomial poly in x"""
+        val = 0.0
+        for coeff in reversed(poly):
+            val = val * x + coeff
+        return val
+
     def test_time(self):
         output = ''
 
         start1 = time.time()
         for j in self.random_numbers:
-            self.first_poly_horner(j)
+            self.poly_horner_eval([0, 1, 0, -self.c[0], 0, self.c[1]], j)
         end1 = time.time()
         print("Efficient first poly time is " + str((end1 - start1)))
         output += "<br/>Efficient First poly time is " + str((end1 - start1))
 
         start2 = time.time()
         for j in self.random_numbers:
-            self.second_poly_horner(j)
+            self.poly_horner_eval([0, 1, 0, -self.c[0], 0, self.c[1], 0, -self.c[2]], j)
         end2 = time.time()
         print("Efficient second poly time is " + str((end2 - start2)))
         output += "<br/>Efficient Second poly time is " + str((end2 - start2))
 
         start3 = time.time()
         for j in self.random_numbers:
-            self.third_poly_horner(j)
+            self.poly_horner_eval([0, 1, 0, -self.c[0], 0, self.c[1], 0, -self.c[2], 0, self.c[3]], j)
         end3 = time.time()
         print("Efficient third poly time is " + str((end3 - start3)))
         output += "<br/>Efficient Third poly time is " + str((end3 - start3))
 
         start4 = time.time()
         for j in self.random_numbers:
-            self.fourth_poly_horner(j)
+            self.poly_horner_eval([0, 1, 0, -0.166, 0, 0.00833, 0, -self.c[2], 0, self.c[3]], j)
         end4 = time.time()
         print("Efficient fourth poly time is " + str((end4 - start4)))
         output += "<br/>Efficient Fourth poly time is " + str((end4 - start4))
 
         start5 = time.time()
         for j in self.random_numbers:
-            self.fifth_poly_horner(j)
+            self.poly_horner_eval([0, 1, 0, -self.c[0], 0, self.c[1], 0, -self.c[2], 0, self.c[3], 0, -self.c[4]], j)
         end5 = time.time()
         print("Efficient fifth poly time is " + str((end5 - start5)))
         output += "<br/>Efficient Fifth poly time is " + str((end5 - start5))
 
         start6 = time.time()
         for j in self.random_numbers:
-            self.sixth_poly_horner(j)
+            self.poly_horner_eval(
+                [0, 1, 0, -self.c[0], 0, self.c[1], 0, -self.c[2], 0, self.c[3], 0, -self.c[4], 0, self.c[5]], j)
         end6 = time.time()
         print("Efficient Sixth poly time is " + str((end6 - start6)))
         output += "<br/>Efficient Sixth poly time is " + str((end6 - start6))
