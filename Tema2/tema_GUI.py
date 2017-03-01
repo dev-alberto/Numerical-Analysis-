6293277e3949d3c0e706109d0b5200139dfc9b74
xR86 <9412441+xR86@users.noreply.github.com>
2017-03-01 23:44:20 +0200

    fixed homework 2 structure, finished interface

--- Tema2/tema_GUI.py
+++ Tema2/tema_GUI.py
@@ -1,26 +1,38 @@
-from gui import GUI
+from tema import LDL_Decomposition
 from sympy import Matrix
 
-stuff = GUI(Matrix([[1, 2.5, 3], [2.5, 8.25, 15.5], [3, 15.5, 43]]), [2, 3, 1], 10**(-10))
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+CH = LDL_Decomposition(Matrix([[1, 2.5, 3], [2.5, 8.25, 15.5], [3, 15.5, 43]]), [2, 3, 1], 10**(-10))
 
 
-### Exercitiul 1 ###
-print(stuff.decompose_wrapper())
+@app.route('/')
+def hello_world():
+    name = 'Flask app'
+    return render_template('index.html', name=name)
 
 
-### Exercitiul 2 ###
-print(stuff.compute_determinant_wrapper())
+# Route handlers
+@app.route('/decompose')
+def decompose():
+	return str(CH.decompose_wrapper())
+
+@app.route('/determinant')
+def determinant():
+	return str(CH.compute_determinant_wrapper())
 
 
-### Exercitiu 3 ###
-print(stuff.solution_wrapper())
+@app.route('/solution')
+def solution():
+	return str(CH.solution_wrapper())
 
+@app.route('/library-solution')
+def library_solution():
+	return str(CH.library_solution_wrapper())
 
-### Exercitiu 4 ###
-print(stuff.library_solution_wrapper())
-
-
-### Exercitiu 5 ###
-print(stuff.check_precision())
-
-
+@app.route('/precision')
+def precision():
+	return str(CH.check_precision())
