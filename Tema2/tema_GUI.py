ba2715e28b8941b51b46da0b183a9f97f4522f77
dev-alberto <alberto.cieri@info.uaic.ro>
2017-03-02 22:47:17 +0200

    Parsing almost finished

--- Tema2/tema_GUI.py
+++ Tema2/tema_GUI.py
@@ -6,7 +6,7 @@ from flask import render_template
 
 app = Flask(__name__)
 
-CH = LDL_Decomposition(Matrix([[1, 2.5, 3], [2.5, 8.25, 15.5], [3, 15.5, 43]]), [2, 3, 1], 10**(-10))
+CH = LDL_Decomposition(Matrix([[1, 2.5, 3], [2.5, 8.25, 15.5], [3, 15.5, 43]]), [2, 3, 1], 10 ** (-10))
 
 
 @app.route('/')
@@ -18,21 +18,24 @@ def hello_world():
 # Route handlers
 @app.route('/decompose')
 def decompose():
-	return str(CH.decompose_wrapper())
+    return str(CH.decompose_wrapper())
+
 
 @app.route('/determinant')
 def determinant():
-	return str(CH.compute_determinant_wrapper())
+    return str(CH.compute_determinant_wrapper())
 
 
 @app.route('/solution')
 def solution():
-	return str(CH.solution_wrapper())
+    return str(CH.solution_wrapper())
+
 
 @app.route('/library-solution')
 def library_solution():
-	return str(CH.library_solution_wrapper())
+    return str(CH.library_solution_wrapper())
+
 
 @app.route('/precision')
 def precision():
-	return str(CH.check_precision())
+    return str(CH.check_precision())
