78e29a4fb0cb92bbcc272066b14c4ef3c2f7fe22
dev-alberto <alberto.cieri@info.uaic.ro>
2017-03-09 00:25:57 +0200

    finished matrix addition

--- Tema3/test.py
+++ Tema3/test.py
@@ -14,19 +14,28 @@ from parser import Parser
 #         if (int(intermediate[i][1]) == int(intermediate[j][1])) and (int(intermediate[i][2].strip('\n')) == int(intermediate[j][2].strip('\n'))):
 #             print("nuuuu")
 
-parser_a = Parser("small.txt")
-parser_b = Parser("small2.txt")
-sum = Parser("smallSum.txt")
+parser_a = Parser("a.txt")
+parser_b = Parser("b.txt")
+suma = Parser("aplusb.txt")
 
 matrix_a = Sparse(parser_a.parse_matrix())
 matrix_b = Sparse(parser_b.parse_matrix())
-good_sum = Sparse(sum.parse_matrix())
+good_sum = Sparse(suma.parse_matrix())
 my_sum = matrix_a + matrix_b
+
+#print(matrix_a)
+
+#print("****")
+
+#print(matrix_b)
+
+print("*****")
 
 print(my_sum)
 
-print("****")
+print("******")
 
 print(good_sum)
 
 assert my_sum == good_sum
+
