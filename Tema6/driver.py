d6aa0ae65fdbb44c0be53ddc228485a730ddd70a
xR86 <dan.alexandru@info.uaic.ro>
2017-05-19 11:43:23 +0300

    updated hw6 GUI

--- Tema6/driver.py
+++ Tema6/driver.py
@@ -2,8 +2,16 @@ from eigen_values import Solver
 
 solver = Solver("m_rar_sim_2017.txt")
 #print(solver.generate_random_v0())
+
+
+#not implemented
 #print(solver.multiply_column_vector_with_line_vector([1, 2, 3], [1, 0, 1]))
+
+# not implemented
 #print(solver.multiply_normal_matrix_with_vector([[1, 0, 1], [2, 0, 2], [3, 0, 3]], [1, 2, 3]))
+	# try: multiply_matrix_with_vector
+
+
 #print(solver.find_largest_eigen_value(rand=True))
 #print(solver.generate_svd_matrix())
 solver.find_svd_eigen_values(10, 9)
--- Tema6/tema_GUI.py
+++ Tema6/tema_GUI.py
@@ -9,12 +9,7 @@ app = Flask(__name__)
 
 
 '''
-solver = Solver("m_rar_sim_2017.txt", 1500)
-print(solver.generate_random_v0())
-print(solver.multiply_column_vector_with_line_vector([1, 2, 3], [1, 0, 1]))
-print(solver.multiply_normal_matrix_with_vector([[1, 0, 1], [2, 0, 2], [3, 0, 3]], [1, 2, 3]))
-
-print(solver.find_largest_eigen_value())
+check driver.py for call examples
 '''
 
 @app.route('/')
@@ -28,7 +23,7 @@ def hello_world():
 @app.route('/hw6/largest_eigen', methods=['GET'])
 def find_largest_eigen_value_wrapper():
 	solver = Solver("m_rar_sim_2017.txt", 1500)
-	return str(solver.find_largest_eigen_value())
+	return str(solver.find_largest_eigen_value()) #rand=True
 
 
 @app.route('/hw6/random_v0', methods=['GET'])
@@ -52,8 +47,9 @@ def multiply_column_vector_with_line_vector_wrapper():
 
 	return str(solver.multiply_column_vector_with_line_vector(eval(col_vector), eval(lin_vector)))
 
+
 @app.route('/hw6/multiply_matrix_vector', methods=['POST'])
-def multiply_normal_matrix_with_vector_wrapper():
+def multiply_matrix_with_vector_wrapper():
 	# [[1, 0, 1], [2, 0, 2], [3, 0, 3]], [1, 2, 3]
 	solver = Solver("m_rar_sim_2017.txt", 1500)
 
@@ -64,7 +60,27 @@ def multiply_normal_matrix_with_vector_wrapper():
 	print(eval(matrix))
 	print(eval(vector))
 
-	return str(solver.multiply_normal_matrix_with_vector(eval(col_vector), eval(lin_vector)))
+	return str(solver.multiply_matrix_with_vector(eval(matrix), eval(vector)))
+
+
+@app.route('/hw6/svd_matrix', methods=['GET'])
+def generate_svd_matrix_wrapper():
+	solver = Solver("m_rar_sim_2017.txt", 1500)
+	return str(solver.generate_svd_matrix())
+
+
+@app.route('/hw6/svd_eigen', methods=['POST'])
+def find_svd_eigen_values_wrapper():
+	solver = Solver("m_rar_sim_2017.txt", 1500)
+
+	print(request.get_json()) #force=True
+	a = request.get_json()["a"]
+	b = request.get_json()["b"]
+
+	print(int(a))
+	print(int(b))
+
+	return str(solver.find_svd_eigen_values(int(a), int(b)))
 
 
 @app.route('/result')
